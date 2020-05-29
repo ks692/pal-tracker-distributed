0ac26f62e98ba8dbb4f9f8228a64097218ca95b1
ks692 <ks692@snu.edu.in>
2020-05-29 10:59:29 +0100

    CB1.0

--- components/allocations/src/main/java/io/pivotal/pal/tracker/allocations/ProjectClient.java
+++ components/allocations/src/main/java/io/pivotal/pal/tracker/allocations/ProjectClient.java
@@ -1,18 +1,32 @@
 package io.pivotal.pal.tracker.allocations;
 
+import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
 import org.springframework.web.client.RestOperations;
+
+import java.util.concurrent.ConcurrentHashMap;
 
 public class ProjectClient {
 
     private final RestOperations restOperations;
     private final String registrationServerEndpoint;
+    private final ConcurrentHashMap<Long,ProjectInfo> internalCache=new ConcurrentHashMap<>();
 
     public ProjectClient(RestOperations restOperations, String registrationServerEndpoint) {
         this.restOperations= restOperations;
         this.registrationServerEndpoint = registrationServerEndpoint;
     }
-
+    @CircuitBreaker(name = "project", fallbackMethod = "getProjectFromCache")
     public ProjectInfo getProject(long projectId) {
-        return restOperations.getForObject(registrationServerEndpoint + "/projects/" + projectId, ProjectInfo.class);
+        System.out.println("getProject"+" "+projectId);
+        ProjectInfo projectInfo=restOperations.getForObject(registrationServerEndpoint + "/projects/" + projectId, ProjectInfo.class);
+        internalCache.put(projectId,projectInfo);
+        return projectInfo;
     }
+    public ProjectInfo getProjectFromCache(long projectId,Throwable cause){
+        System.out.println("getProjectFromCache"+" "+projectId);
+
+        return internalCache.get(projectId);
+    }
+
+
 }
